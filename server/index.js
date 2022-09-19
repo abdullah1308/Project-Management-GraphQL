b0b1294411c107883fa1e1bb922edc72f98d0869
abdullah1308 <abdullahrafi.1308@gmail.com>
2022-09-19 20:36:26 +0530

    Completed Project

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const colors = require('colors');
+const colors = require("colors");
+const cors = require("cors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,10 +12,14 @@ const app = express();
 // Connect to database
 connectDB();
 
-app.use("/graphql", graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development'
+app.use(cors());
 
-}));
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === "development",
+    })
+);
 
 app.listen(port, console.log(`Server started on port ${port}`));
